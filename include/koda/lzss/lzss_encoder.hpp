335c9d3e0083c93516d107657ef592d6b7c6e04c
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-07 23:15:48 +0100

    Updated lzss encoder header

--- include/koda/lzss/lzss_encoder.hpp
+++ include/koda/lzss/lzss_encoder.hpp
@@ -1,23 +1,26 @@
 #pragma once
 
 #include <koda/encoder_base.hpp>
+#include <koda/lz/lz_distance_table.hpp>
 
 #include <memory>
 
 namespace koda {
 
-class LzssEncoder final : public EncoderBase<uint8_t, bool> {
+class LzssEncoder final : public EncoderBase<uint8_t> {
    public:
-    explicit LzssEncoder(std::unique_ptr<EncoderBase<uint16_t, bool>>
+    explicit LzssEncoder(std::unique_ptr<LzDistanceTable> distance_table,
+                         std::unique_ptr<EncoderBase<uint16_t>>
                              auxiliary_encoder = nullptr) noexcept;
 
     void operator()(InputTokenStream& input,
-                    OutputTokenStream& output) const override final;
+                    OutputBinaryStream& output) const override final;
 
     ~LzssEncoder() noexcept override final = default;
 
    private:
-    std::unique_ptr<EncoderBase<uint16_t, bool>> auxiliary_encoder_;
+    std::unique_ptr<LzDistanceTable> distance_table_;
+    std::unique_ptr<EncoderBase<uint16_t>> auxiliary_encoder_;
 };
 
 }  // namespace koda
