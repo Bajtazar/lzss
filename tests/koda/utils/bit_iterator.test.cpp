e1be17926c07db1c63f9f0723af046173a8e1fdc
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-08 00:09:05 +0100

    Added position to tests

--- tests/koda/utils/bit_iterator.test.cpp
+++ tests/koda/utils/bit_iterator.test.cpp
@@ -10,14 +10,23 @@ TEST(LittleEndianInputBitIterTest, AppendBits) {
     std::vector<uint8_t> bytes = {0b101011};
     koda::LittleEndianInputBitIter iter{bytes.cbegin()};
 
+    ASSERT_EQ(iter.Position(), 0);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 1);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 2);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 3);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 4);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 5);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 6);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 7);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 0);
 
     ASSERT_NE(iter, koda::LittleEndianInputBitIter{bytes.cbegin()});
 }
@@ -26,14 +35,23 @@ TEST(BigEndianInputBitIterTest, AppendBits) {
     std::vector<uint8_t> bytes = {0b101011};
     koda::BigEndianInputBitIter iter{bytes.cbegin()};
 
+    ASSERT_EQ(iter.Position(), 7);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 6);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 5);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 4);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 3);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 2);
     ASSERT_FALSE(*iter++);
+    ASSERT_EQ(iter.Position(), 1);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 0);
     ASSERT_TRUE(*iter++);
+    ASSERT_EQ(iter.Position(), 7);
 
     ASSERT_NE(iter, koda::BigEndianInputBitIter{bytes.cbegin()});
 }
