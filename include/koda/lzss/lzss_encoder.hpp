50d93063ea0b5c1a073b8af9d0af64f321009490
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-08 23:51:26 +0100

    Removed lz distance table

--- include/koda/lzss/lzss_encoder.hpp
+++ include/koda/lzss/lzss_encoder.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <koda/coder.hpp>
-#include <koda/lz/lz_distance_table.hpp>
 #include <koda/utils/concepts.hpp>
 
 #include <memory>
@@ -42,19 +41,16 @@ class [[nodiscard]] IntermediateToken {
 
 template <std::integral InputToken = uint8_t,
           std::integral IntermediateToken = uint16_t,
-          LzDistanceTable<IntermediateToken> DistanteTable,
           Encoder<IntermediateToken> AuxiliaryEncoder>
     requires(sizeof(InputToken) <= sizeof(IntermediateToken))
 class LzssEncoder {
    public:
-    constexpr explicit LzssEncoder(DistanteTable distance_table,
-                                   AuxiliaryEncoder auxiliary_encoder) noexcept;
+    constexpr explicit LzssEncoder(AuxiliaryEncoder auxiliary_encoder) noexcept;
 
     constexpr void operator()(auto InputRange<InputToken>&& input,
                               auto BitOutputRange&& output) const;
 
    private:
-    DistanteTable distance_table_;
     AuxiliaryEncoder auxiliary_encoder_;
 };
 
