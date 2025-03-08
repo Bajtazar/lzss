f1852d55d340349d50213c03976ccd3bbe985054
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-08 01:52:23 +0100

    Updated lz distance table

--- include/koda/lz/lz_distance_table.hpp
+++ include/koda/lz/lz_distance_table.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <koda/decoder_base.hpp>
-#include <koda/encoder_base.hpp>
+#include <koda/coder.hpp>
 
 #include <cstdlib>
 #include <istream>
@@ -9,35 +8,17 @@
 
 namespace koda {
 
-class LzDistanceTable {
-   public:
-    using InputBinaryStream = std::basic_istream<bool>;
-    using OutputBinaryStream = std::basic_ostream<bool>;
-
-    struct [[nodiscard]] RepeatitionPosition {
-        size_t distance;
-        size_t match_length;
-    };
-
-    constexpr explicit LzDistanceTable() noexcept = default;
-
-    virtual void EncodeDistance(
-        OutputBinaryStream& stream, size_t distance, size_t match_length,
-        const EncoderBase<uint16_t>& auxiliary_encoder) const = 0;
-
-    virtual void DecodeDistance(
-        InputBinaryStream& stream, uint16_t start_token,
-        const DecoderBase<uint16_t>& auxiliary_decoder) const = 0;
-
-    virtual ~LzDistanceTable() noexcept = default;
-
-   protected:
-    constexpr LzDistanceTable(const LzDistanceTable&) noexcept = default;
-    constexpr LzDistanceTable(LzDistanceTable&&) noexcept = default;
-
-    constexpr LzDistanceTable& operator=(const LzDistanceTable&) noexcept =
-        default;
-    constexpr LzDistanceTable& operator=(LzDistanceTable&&) noexcept = default;
+template <typename Tp, typename IntermediateToken>
+concept LzDistanceTable = requires(
+    Tp table, DummyEncoder encoder, DummyDecoder<IntermediateToken> decoder,
+    size_t distance, size_t match_length, IntermediateToken start_token,
+    DummyBitInputRange input, DummyBitOutputRange output) {
+    {
+        table.EncodeDistance(output, distance, match_length, encoder)
+    } -> std::same_as<void>;
+    {
+        table.DecodeDistance(input, start_token, decoder)
+    } -> std::same_as<std::pair<size_t, size_t>>;
 };
 
 }  // namespace koda
