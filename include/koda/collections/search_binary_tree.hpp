86a4c1735efe45a6d82954430bac4ff088c55aed
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-09 23:03:17 +0100

    Added search binary tree

--- include/koda/collections/search_binary_tree.hpp
+++ include/koda/collections/search_binary_tree.hpp
@@ -24,15 +24,26 @@ class SearchBinaryTree {
 
     void RemoveString(StringView string);
 
-    RepeatitionMarker FindMatch(StringView buffer);
+    RepeatitionMarker FindMatch(StringView buffer) const;
 
    private:
+    // Allows to search using string_view
+    struct TransparentComparator : public std::less<> {
+        using is_transparent = void;
+
+        using std::less<>::operator();
+    };
+
     using String = std::basic_string<uint8_t>;
-    using BinaryTree = std::map<String, std::pair<size_t, size_t>>;
+    using BinaryTree =
+        std::map<String, std::pair<size_t, size_t>, TransparentComparator>;
 
     BinaryTree tree_;
     size_t dictionary_start_index_ = 0;
     size_t buffer_start_index_ = 0;
+
+    static size_t FindCommonPrefixSize(StringView buffer,
+                                       StringView node) noexcept;
 };
 
 }  // namespace koda
---
+++ src/koda/collections/search_binary_tree.cpp
@@ -0,0 +1,66 @@
+#include <koda/collections/search_binary_tree.hpp>
+
+#include <algorithm>
+#include <cassert>
+#include <ranges>
+
+namespace koda {
+
+void SearchBinaryTree::AddString(StringView string) {
+    auto iter = tree_.find(string);
+    if (iter != tree_.end()) {
+        // Increase a reference counter of this string
+        ++(iter->second.second);
+    } else {
+        // String is copied here since it can outlive its creator in circular
+        // buffer
+        tree_.emplace(string, std::forward_as_tuple(buffer_start_index_, 1));
+    }
+    ++buffer_start_index_;
+}
+
+void SearchBinaryTree::RemoveString(StringView string) {
+    auto iter = tree_.find(string);
+    assert(iter != tree_.end() &&
+           "String that is not part of the tree has been given");
+
+    // If it is present only one time in the dictionary then delete
+    if (iter->second.second == 1) {
+        tree_.erase(iter);
+    } else {
+        // Decrease a reference counter
+        ++(iter->second.second);
+    }
+
+    ++dictionary_start_index_;
+}
+
+SearchBinaryTree::RepeatitionMarker SearchBinaryTree::FindMatch(
+    StringView buffer) const {
+    auto iter = tree_.lower_bound(buffer);
+    if (iter == tree_.end()) {
+        // No string has been found
+        return {0, 0};
+    }
+    const auto common_length = FindCommonPrefixSize(buffer, iter->first);
+    if (common_length == 0) {
+        return {0, 0};
+    }
+    return {// Calculate relative offset from the start of the dictionary
+            iter->second.first - dictionary_start_index_, common_length};
+}
+
+/*static*/ size_t SearchBinaryTree::FindCommonPrefixSize(
+    StringView buffer, StringView node) noexcept {
+    for (auto [index, comp] :
+         std::views::enumerate(std::views::zip(buffer, node))) {
+        const auto& [left, right] = comp;
+        if (left != right) {
+            return index;
+        }
+    }
+    [[assume(buffer.size() <= node.size())]];
+    return buffer.size();
+}
+
+}  // namespace koda
