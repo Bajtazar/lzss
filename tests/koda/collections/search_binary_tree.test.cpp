cc5136bb9e72fe7a62e7c0bbf3879d2d4820cb09
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-09 23:37:14 +0100

    Started working on search binary tree tests

---
+++ tests/koda/collections/search_binary_tree.test.cpp
@@ -0,0 +1,39 @@
+#include <koda/collections/search_binary_tree.hpp>
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <cstring>
+#include <ranges>
+
+template <std::ranges::input_range Range>
+static std::basic_string<uint8_t> ConvertToString(Range&& range) {
+    std::basic_string<uint8_t> result;
+    std::ranges::transform(
+        std::forward<Range>(range), std::back_inserter(result),
+        [](const char token) { return static_cast<uint8_t>(token); });
+    return result;
+}
+
+template <size_t Window, size_t Length>
+    requires(Length >= Window)
+static std::array<std::basic_string<uint8_t>, Length - Window>
+BuildSamplesFromString(const char (&sentence)[Length]) {
+    std::array<std::basic_string<uint8_t>, Length - Window> result;
+    for (size_t i = 0; i < result.size(); ++i) {
+        result[i] = ConvertToString(
+            std::ranges::subrange{&sentence[i], &sentence[i + Window]});
+    }
+    return result;
+}
+
+#include <iostream>
+
+static const auto kSampleString =
+    BuildSamplesFromString<4>("ala ma kota a kot ma ale");
+
+static const auto kOneOfRepeated = ConvertToString(" kot");
+
+TEST(SearchBinaryTreeTest, Creation) { koda::SearchBinaryTree tree; }
