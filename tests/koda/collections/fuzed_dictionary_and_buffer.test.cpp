56daaa50e57d1f4f1fb5207a1a989a40cf56ff68
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-06-06 22:38:15 +0200

    Updated fuzed dict tests

--- tests/koda/collections/fuzed_dictionary_and_buffer.test.cpp
+++ tests/koda/collections/fuzed_dictionary_and_buffer.test.cpp
@@ -6,7 +6,6 @@
 #include <algorithm>
 #include <array>
 #include <cstring>
-#include <random>
 #include <ranges>
 
 #include "viewable_vector.hpp"
@@ -74,20 +73,24 @@ BeginConstexprTest(FuzedDictionaryAndBufferTest, SimpleBufferAccomodation) {
 }
 EndConstexprTest(FuzedDictionaryAndBufferTest, SimpleBufferAccomodation);
 
-static std::vector<uint8_t> GeneratePseudoNumberSequence(size_t length) {
-    std::mt19937 engine;
-    std::uniform_int_distribution<uint8_t> distibution{0, 255};
+static constexpr std::vector<uint8_t> GeneratePseudoNumberSequence(
+    size_t length) {
+    static constexpr unsigned kAlpha = 16807;
+    static constexpr unsigned kModulo = 2147483647;
+    size_t value = 43948390;
 
     std::vector<uint8_t> result;
     result.resize(length);
 
-    std::ranges::for_each(
-        result, [&](auto& element) { element = distibution(engine); });
+    std::ranges::for_each(result, [&](auto& element) {
+        element = static_cast<uint8_t>(value);
+        value = (kAlpha * value) % kModulo;
+    });
 
     return result;
 }
 
-TEST(FuzedDictionaryAndBufferTest, LongRunBufferAndDict) {
+BeginConstexprTest(FuzedDictionaryAndBufferTest, LongRunBufferAndDict) {
     koda::FusedDictionaryAndBuffer dict{kDictSize, kBufferView};
     // stress test
     auto sequence = GeneratePseudoNumberSequence(kRepeatitons);
@@ -99,9 +102,10 @@ TEST(FuzedDictionaryAndBufferTest, LongRunBufferAndDict) {
     auto buff_iter = 0;
     for (auto const& elem : sequence | std::views::drop(kBufferView.size()) |
                                 std::views::take(kDictSize)) {
-        ASSERT_EQ(dict.get_buffer(),
-                  AsString(sequence[buff_iter], sequence[buff_iter + 1],
-                           sequence[buff_iter + 2], sequence[buff_iter + 3]));
+        ConstexprEqual(
+            dict.get_buffer(),
+            AsString(sequence[buff_iter], sequence[buff_iter + 1],
+                     sequence[buff_iter + 2], sequence[buff_iter + 3]));
         ++buff_iter;
         dict.AddSymbolToBuffer(elem);
     }
@@ -109,19 +113,21 @@ TEST(FuzedDictionaryAndBufferTest, LongRunBufferAndDict) {
     auto iter = 0;
     for (auto const& elem :
          sequence | std::views::drop(kDictSize + kBufferView.size())) {
-        ASSERT_EQ(dict.get_buffer(),
-                  AsString(sequence[buff_iter], sequence[buff_iter + 1],
-                           sequence[buff_iter + 2], sequence[buff_iter + 3]));
-        ASSERT_EQ(dict.get_oldest_dictionary_full_match(),
-                  AsString(sequence[iter], sequence[iter + 1],
-                           sequence[iter + 2], sequence[iter + 3]));
+        ConstexprEqual(
+            dict.get_buffer(),
+            AsString(sequence[buff_iter], sequence[buff_iter + 1],
+                     sequence[buff_iter + 2], sequence[buff_iter + 3]));
+        ConstexprEqual(dict.get_oldest_dictionary_full_match(),
+                       AsString(sequence[iter], sequence[iter + 1],
+                                sequence[iter + 2], sequence[iter + 3]));
         ++iter;
         ++buff_iter;
         dict.AddSymbolToBuffer(elem);
     }
 }
+EndConstexprTest(FuzedDictionaryAndBufferTest, LongRunBufferAndDict);
 
-TEST(FuzedDictionaryAndBufferTest, AddEndSymbolTests) {
+BeginConstexprTest(FuzedDictionaryAndBufferTest, AddEndSymbolTests) {
     koda::FusedDictionaryAndBuffer dict{kDictSize, kBufferView};
     // stress test
     auto sequence =
@@ -134,36 +140,41 @@ TEST(FuzedDictionaryAndBufferTest, AddEndSymbolTests) {
     // Now N-last element of dict are N-last elements of the sequence!
     dict.AddEndSymbolToBuffer();
 
-    ASSERT_EQ(dict.get_oldest_dictionary_full_match(),
-              AsString(sequence[1], sequence[2], sequence[3], sequence[4]));
-    ASSERT_EQ(dict.get_buffer(),
-              AsString(sequence[kDictSize + 1], sequence[kDictSize + 2],
-                       sequence[kDictSize + 3]));
-    ASSERT_EQ(dict.dictionary_size(), dict.max_dictionary_size());
-    ASSERT_EQ(dict.buffer_size(), dict.max_buffer_size() - 1);
+    ConstexprEqual(
+        dict.get_oldest_dictionary_full_match(),
+        AsString(sequence[1], sequence[2], sequence[3], sequence[4]));
+    ConstexprEqual(dict.get_buffer(),
+                   AsString(sequence[kDictSize + 1], sequence[kDictSize + 2],
+                            sequence[kDictSize + 3]));
+    ConstexprEqual(dict.dictionary_size(), dict.max_dictionary_size());
+    ConstexprEqual(dict.buffer_size(), dict.max_buffer_size() - 1);
 
     dict.AddEndSymbolToBuffer();
 
-    ASSERT_EQ(dict.get_oldest_dictionary_full_match(),
-              AsString(sequence[2], sequence[3], sequence[4], sequence[5]));
-    ASSERT_EQ(dict.get_buffer(),
-              AsString(sequence[kDictSize + 2], sequence[kDictSize + 3]));
-    ASSERT_EQ(dict.dictionary_size(), dict.max_dictionary_size());
-    ASSERT_EQ(dict.buffer_size(), dict.max_buffer_size() - 2);
+    ConstexprEqual(
+        dict.get_oldest_dictionary_full_match(),
+        AsString(sequence[2], sequence[3], sequence[4], sequence[5]));
+    ConstexprEqual(dict.get_buffer(),
+                   AsString(sequence[kDictSize + 2], sequence[kDictSize + 3]));
+    ConstexprEqual(dict.dictionary_size(), dict.max_dictionary_size());
+    ConstexprEqual(dict.buffer_size(), dict.max_buffer_size() - 2);
 
     dict.AddEndSymbolToBuffer();
 
-    ASSERT_EQ(dict.get_oldest_dictionary_full_match(),
-              AsString(sequence[3], sequence[4], sequence[5], sequence[6]));
-    ASSERT_EQ(dict.get_buffer(), AsString(sequence[kDictSize + 3]));
-    ASSERT_EQ(dict.dictionary_size(), dict.max_dictionary_size());
-    ASSERT_EQ(dict.buffer_size(), dict.max_buffer_size() - 3);
+    ConstexprEqual(
+        dict.get_oldest_dictionary_full_match(),
+        AsString(sequence[3], sequence[4], sequence[5], sequence[6]));
+    ConstexprEqual(dict.get_buffer(), AsString(sequence[kDictSize + 3]));
+    ConstexprEqual(dict.dictionary_size(), dict.max_dictionary_size());
+    ConstexprEqual(dict.buffer_size(), dict.max_buffer_size() - 3);
 
     dict.AddEndSymbolToBuffer();
 
-    ASSERT_EQ(dict.get_oldest_dictionary_full_match(),
-              AsString(sequence[4], sequence[5], sequence[6], sequence[7]));
-    ASSERT_EQ(dict.get_buffer(), std::basic_string_view<uint8_t>{});
-    ASSERT_EQ(dict.dictionary_size(), dict.max_dictionary_size());
-    ASSERT_EQ(dict.buffer_size(), 0);
+    ConstexprEqual(
+        dict.get_oldest_dictionary_full_match(),
+        AsString(sequence[4], sequence[5], sequence[6], sequence[7]));
+    ConstexprEqual(dict.get_buffer(), std::basic_string_view<uint8_t>{});
+    ConstexprEqual(dict.dictionary_size(), dict.max_dictionary_size());
+    ConstexprEqual(dict.buffer_size(), 0);
 }
+EndConstexprTest(FuzedDictionaryAndBufferTest, AddEndSymbolTests);
