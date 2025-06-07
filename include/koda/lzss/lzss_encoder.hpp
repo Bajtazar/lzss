abe86d96ffc44f0a54c1a3a4ac5ca6e6e4caab01
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-06-07 20:09:24 +0200

    Moved lzss intermediate token outside main encoder

--- include/koda/lzss/lzss_encoder.hpp
+++ include/koda/lzss/lzss_encoder.hpp
@@ -10,37 +10,6 @@
 
 namespace koda {
 
-class [[nodiscard]] LzssIntermediateToken {
-   public:
-    struct RepeatitionMarker {
-        size_t match_position;
-        size_t match_length;
-    };
-
-    constexpr explicit LzssIntermediateToken(uint8_t symbol) noexcept;
-
-    constexpr explicit LzssIntermediateToken(size_t match_position,
-                                             size_t match_length) noexcept;
-
-    [[nodiscard]] constexpr bool holds_symbol() const noexcept;
-
-    [[nodiscard]] constexpr bool holds_marker() const noexcept;
-
-    [[nodiscard]] constexpr std::optional<uint8_t> get_symbol() const noexcept;
-
-    [[nodiscard]] constexpr std::optional<RepeatitionMarker> get_marker()
-        const noexcept;
-
-   private:
-    bool holds_distance_match_pair_;
-    // We don't need to manually call destructor on an active
-    // member since both of them are trivially destructible
-    union {
-        uint8_t symbol_;
-        RepeatitionMarker repeatition_marker_;
-    };
-};
-
 template <std::integral InputToken = uint8_t,
           Encoder<LzssIntermediateToken> AuxiliaryEncoder>
     requires(sizeof(InputToken) <= sizeof(LzssIntermediateToken))
@@ -66,4 +35,4 @@ class LzssEncoder {
 
 }  // namespace koda
 
-#include <koda/lzss/lzss_encoder.ipp>
+#include <koda/lzss/lzss_encoder.tpp>
---
+++ include/koda/lzss/lzss_intermediate_token.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cinttypes>
+#include <cstddef>
+#include <optional>
+
+namespace koda {
+
+class [[nodiscard]] LzssIntermediateToken {
+   public:
+    struct RepeatitionMarker {
+        size_t match_position;
+        size_t match_length;
+    };
+
+    constexpr explicit LzssIntermediateToken(uint8_t symbol) noexcept;
+
+    constexpr explicit LzssIntermediateToken(size_t match_position,
+                                             size_t match_length) noexcept;
+
+    [[nodiscard]] constexpr bool holds_symbol() const noexcept;
+
+    [[nodiscard]] constexpr bool holds_marker() const noexcept;
+
+    [[nodiscard]] constexpr std::optional<uint8_t> get_symbol() const noexcept;
+
+    [[nodiscard]] constexpr std::optional<RepeatitionMarker> get_marker()
+        const noexcept;
+
+   private:
+    bool holds_distance_match_pair_;
+    // We don't need to manually call destructor on an active
+    // member since both of them are trivially destructible
+    union {
+        uint8_t symbol_;
+        RepeatitionMarker repeatition_marker_;
+    };
+};
+
+}  // namespace koda
+
+#include <koda/lzss/lzss_intermediate_token.tpp>
