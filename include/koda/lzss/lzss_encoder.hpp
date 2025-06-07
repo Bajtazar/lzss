1a5198d987d178c9a849851fb9037ca93384bbf4
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-06-07 20:16:10 +0200

    Added lzss encoder ctor

--- include/koda/lzss/lzss_encoder.hpp
+++ include/koda/lzss/lzss_encoder.hpp
@@ -3,21 +3,26 @@
 #include <koda/coder.hpp>
 #include <koda/collections/fuzed_dictionary_and_buffer.hpp>
 #include <koda/collections/search_binary_tree.hpp>
+#include <koda/lzss/lzss_intermediate_token.hpp>
 #include <koda/utils/concepts.hpp>
 
+#include <concepts>
 #include <memory>
 #include <optional>
 
 namespace koda {
 
 template <std::integral InputToken = uint8_t,
-          Encoder<LzssIntermediateToken> AuxiliaryEncoder>
+          Encoder<LzssIntermediateToken> AuxiliaryEncoder,
+          typename AllocatorTp = std::allocator<InputToken>>
     requires(sizeof(InputToken) <= sizeof(LzssIntermediateToken))
 class LzssEncoder {
    public:
     constexpr explicit LzssEncoder(
-        size_t dictionary_size,
-        AuxiliaryEncoder auxiliary_encoder = {}) noexcept;
+        size_t dictionary_size, size_t look_ahead_size,
+        AuxiliaryEncoder auxiliary_encoder,
+        std::optional<size_t> cyclic_buffer_size = std::nullopt,
+        const AllocatorTp& allocator = AllocatorTp{});
 
     constexpr void Encode(InputRange<InputToken> auto&& input,
                           BitOutputRange auto&& output);
--- include/koda/lzss/lzss_encoder.tpp
+++ include/koda/lzss/lzss_encoder.tpp
@@ -1,3 +1,17 @@
 #pragma once
 
-namespace koda {}  // namespace koda
+namespace koda {
+
+template <std::integral InputToken,
+          Encoder<LzssIntermediateToken> AuxiliaryEncoder, typename AllocatorTp>
+    requires(sizeof(InputToken) <= sizeof(LzssIntermediateToken))
+constexpr LzssEncoder<InputToken, AuxiliaryEncoder, AllocatorTp>::LzssEncoder(
+    size_t dictionary_size, size_t look_ahead_size,
+    AuxiliaryEncoder auxiliary_encoder,
+    std::optional<size_t> cyclic_buffer_size, const AllocatorTp& allocator)
+    : dictionary_and_buffer_{dictionary_size, look_ahead_size,
+                             std::move(cyclic_buffer_size), allocator},
+      search_tree_{look_ahead_size, allocator},
+      auxiliary_encoder_{std::move(auxiliary_encoder)} {}
+
+}  // namespace koda
