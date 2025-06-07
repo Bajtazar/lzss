cc1e23f25c0af648bb1739b8fb951101fa7d6987
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-06-07 21:22:50 +0200

    Started working on token traits

---
+++ include/koda/coders/token_traits.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <koda/utils/concepts.hpp>
+#include <koda/utils/type_dummies.hpp>
+
+#include <cinttypes>
+#include <concepts>
+
+namespace koda {
+
+template <typename Traits>
+concept TokenTraitsType =
+    requires { Traits::TokenType; } &&
+    requires(typename Traits::TokenType token, DummyBitInputRange input,
+             DummyBitOutputRange output) {
+        { Traits::EncodeToken(token, input) } -> std::same_as<void>;
+        {
+            Traits::DecodeToken(output)
+        } -> std::same_as<typename Traits::TokenType>;
+    };
+
+template <typename Token>
+struct TokenTraits;
+
+template <std::integral Token>
+struct TokenTraits<Token> {
+    using TokenType = Token;
+
+    static constexpr void EncodeToken(TokenType token,
+                                      BitOutputRange auto&& output);
+
+    [[nodiscard]] constexpr TokenType DecodeToken(BitInputRange auto&& input);
+};
+
+}  // namespace koda
