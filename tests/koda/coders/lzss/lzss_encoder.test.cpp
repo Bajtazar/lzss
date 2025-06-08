fffd499a311f3718292685b8a670ddba0e807cae
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-06-08 21:23:34 +0200

    Added more complex case

--- tests/koda/coders/lzss/lzss_encoder.test.cpp
+++ tests/koda/coders/lzss/lzss_encoder.test.cpp
@@ -8,10 +8,14 @@
 namespace {
 template <typename Tp>
 struct LzssDummyAuxEncoder {
+    bool should_pass_all_markers = true;
     std::vector<Tp> tokens = {};
 
     constexpr float TokenBitSize(Tp token) const {
-        return token.holds_symbol() ? 1.f : 0.f;  // save all markers
+        if (should_pass_all_markers) {
+            return token.holds_symbol() ? 1.f : 0.f;  // save all markers
+        }
+        return koda::TokenTraits<Tp>::TokenBitSize(token);
     }
 
     constexpr void Encode(koda::InputRange<Tp> auto&& input,
@@ -29,7 +33,7 @@ struct LzssDummyAuxEncoder {
 
 }  // namespace
 
-BeginConstexprTest(LzssEncoder, EncodeBytes) {
+BeginConstexprTest(LzssEncoder, EncodeTokens) {
     std::string input_sequence = "ala ma kota a kot ma ale";
     std::vector expected_result = {
         koda::LzssIntermediateToken<char>{'a'},
@@ -62,5 +66,45 @@ BeginConstexprTest(LzssEncoder, EncodeBytes) {
 
     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens, expected_result);
     ConstexprAssertTrue(target.empty());
-};
-EndConstexprTest(LzssEncoder, EncodeBytes);
+}
+EndConstexprTest(LzssEncoder, EncodeTokens);
+
+BeginConstexprTest(LzssEncoder, EncodeTokensRealScenario) {
+    std::string input_sequence = "std::nullptr_t & nullptr";
+    std::vector expected_result = {
+        koda::LzssIntermediateToken<char>{'s'},
+        koda::LzssIntermediateToken<char>{'t'},
+        koda::LzssIntermediateToken<char>{'d'},
+        koda::LzssIntermediateToken<char>{':'},
+        koda::LzssIntermediateToken<char>{':'},
+        koda::LzssIntermediateToken<char>{'n'},
+        koda::LzssIntermediateToken<char>{'u'},
+        koda::LzssIntermediateToken<char>{'l'},
+        koda::LzssIntermediateToken<char>{'l'},
+        koda::LzssIntermediateToken<char>{'p'},
+        koda::LzssIntermediateToken<char>{'t'},
+        koda::LzssIntermediateToken<char>{'r'},
+        koda::LzssIntermediateToken<char>{'_'},
+        koda::LzssIntermediateToken<char>{'t'},
+        koda::LzssIntermediateToken<char>{' '},
+        koda::LzssIntermediateToken<char>{'&'},
+        koda::LzssIntermediateToken<char>{' '},
+        koda::LzssIntermediateToken<char>{5, 7},  // 'nullptr'
+    };
+
+    std::vector<uint8_t> target;
+    auto source =
+        koda::MakeLittleEndianOutputSource(koda::BackInserterIterator{target});
+    std::ranges::subrange output_range{koda::LittleEndianOutputBitIter{source},
+                                       std::default_sentinel};
+
+    koda::LzssEncoder<char,
+                      LzssDummyAuxEncoder<koda::LzssIntermediateToken<char>>>
+        encoder{1024, 7,
+                LzssDummyAuxEncoder<koda::LzssIntermediateToken<char>>{false}};
+    encoder(input_sequence, output_range);
+
+    ConstexprAssertEqual(encoder.auxiliary_encoder().tokens, expected_result);
+    ConstexprAssertTrue(target.empty());
+}
+EndConstexprTest(LzssEncoder, EncodeTokensRealScenario);
