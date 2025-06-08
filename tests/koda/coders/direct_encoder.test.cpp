1f8aa0493f467313b897589d801d0300aca67074
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-06-08 17:26:08 +0200

    Started working on direct encoders tests

--- tests/koda/coders/direct_encoder.test.cpp
+++ tests/koda/coders/direct_encoder.test.cpp
@@ -5,57 +5,80 @@
 
 #include <gtest/gtest.h>
 
+#include <bitset>
 #include <iterator>
 #include <vector>
 
-namespace {
-
-class SimpleInputBinaryRange {
-   public:
-    using BitIter =
-        koda::LittleEndianInputBitIter<typename std::vector<uint8_t>::iterator>;
-
-    constexpr explicit SimpleInputBinaryRange(std::vector<uint8_t> vector)
-        : vector_{std::move(vector)}, input_iter_{vector_.begin()} {}
-
-    constexpr BitIter& begin() const noexcept { return input_iter_; }
-
-    constexpr BitIter end() const noexcept { return BitIter{}; }
-
-   private:
-    std::vector<uint8_t> vector_;
-    mutable BitIter input_iter_;
-};
-
-class SimpleOutputBinaryRange {
-   public:
-    using BackInserter = koda::BackInserterIterator<std::vector<uint8_t>>;
-    using BitIter = koda::LittleEndianOutputBitIter<BackInserter>;
-
-    constexpr explicit SimpleOutputBinaryRange()
-        : output_iter_{BackInserter{vector}} {}
-
-    constexpr BitIter& begin() noexcept { return output_iter_; }
-
-    constexpr std::default_sentinel_t end() noexcept {
-        return std::default_sentinel;
+void Print(std::vector<uint8_t> v) {
+    for (auto k : v) {
+        std::cout << std::bitset<8>{k} << ", ";
     }
+    std::cout << "\n";
+}
 
-    std::vector<uint8_t> vector;
-
-   private:
-    BitIter output_iter_;
-};
-
-}  // namespace
-
-BeginConstexprTest(DirectEncoderTest, EncodeBytes) {
+TEST(DirectEncoderTest, EncodeBytes) {
     const std::vector<uint8_t> expected{{0x43, 0x74, 0x35, 0x33}};
-    SimpleOutputBinaryRange range;
+    std::vector<uint8_t> target;
+    auto source =
+        koda::MakeLittleEndianOutputSource(koda::BackInserterIterator{target});
+    std::ranges::subrange range{koda::LittleEndianOutputBitIter{source},
+                                std::default_sentinel};
+
     koda::DirectEncoder<uint8_t> encoder;
 
     encoder.Encode(expected, range);
 
-    ConstexprAssertEqual(expected, range.vector);
+    Print(expected);
+    Print(target);
+    // ConstexprAssertEqual(expected, target);
 }
-EndConstexprTest(DirectEncoderTest, EncodeBytes);
+// EndConstexprTest(DirectEncoderTest, EncodeBytes);
+
+static constexpr std::vector<uint8_t> ShiftBits(std::vector<uint8_t> vector,
+                                                uint8_t shift) {
+    std::vector<uint8_t> result;
+    uint8_t next = 0;
+    for (auto element : vector) {
+        result.push_back((element << shift) | next);
+        next = element >> shift;
+    }
+    result.push_back(next);
+    return result;
+}
+
+TEST(DirectEncoderTest, EncodeBytesWithStride) {
+    const std::vector<uint8_t> start{
+        {0x43, 0x74, 0x35, 0x33, 0x54, 0x01, 0x84}};
+    std::vector<uint8_t> expected = ShiftBits(start, 4);
+    expected.front() |= 0b100;
+    expected.back() |= 0b10110000;
+
+    std::vector<uint8_t> target;
+    auto source =
+        koda::MakeLittleEndianOutputSource(koda::BackInserterIterator{target});
+    std::ranges::subrange range{koda::LittleEndianOutputBitIter{source},
+                                std::default_sentinel};
+
+    koda::DirectEncoder<uint8_t> encoder;
+
+    auto iter = range.begin();
+    *iter++ = 0;
+    *iter++ = 0;
+    *iter++ = 1;
+    *iter++ = 0;
+
+    encoder.Encode(start, range);
+
+    auto iter2 = range.begin();
+    *iter2++ = 1;
+    *iter2++ = 1;
+    *iter2++ = 0;
+    *iter2++ = 1;
+
+    Print(start);
+    Print(expected);
+    Print(target);
+
+    // ConstexprAssertEqual(expected, range.vector);
+}
+// EndConstexprTest(DirectEncoderTest, EncodeBytes);
