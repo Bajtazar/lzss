20a3922fef799759a4270db2586e46c796d98aff
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-10 16:01:51 +0200

    Started working on huffman table maker

--- include/koda/coders/huffman/huffman_table.hpp
+++ include/koda/coders/huffman/huffman_table.hpp
@@ -4,6 +4,7 @@
 
 #include <cinttypes>
 #include <concepts>
+#include <variant>
 #include <vector>
 
 namespace koda {
@@ -20,6 +21,48 @@ template <std::integral Token,
               std::allocator<std::pair<const Token, std::vector<bool>>>>
 using HuffmanTable =
     Map<Token, std::vector<bool>, HuffmanTableEntryComparator, AllocatorTp>;
+
+template <std::integral Token, std::integral CountTp>
+[[nodiscard]] constexpr HuffmanTable<Token> MakeHuffmanTable(
+    const Map<Token, CountTp>& count);
+
+namespace details {
+
+template <std::integral Token, std::integral CountTp>
+class MakeHuffmanTableFn {
+   public:
+    explicit constexpr MakeHuffmanTableFn(const Map<Token, CountTp>& count)
+        : count_{} {
+        for (const auto& [token, occurences] : count) {
+            if (auto iter = count_.Find(occurences); iter != count_.end()) {
+                iter->second.emplace_back(token);
+            } else {
+                count_.Emplace(occurences, token);
+            }
+        }
+    }
+
+    constexpr HuffmanTable<Token>&& table() && {}
+
+   private:
+    struct Node {
+        using NodeOrLeaf = std::variant<std::unique_ptr<Node>, Token>;
+
+        NodeOrLeaf left;
+        NodeOrLeaf right;
+        Node* parent = nullptr;
+    };
+
+    using NodeOrLeaf = Node::NodeOrLeaf;
+
+    Map<CountTp, std::vector<NodeOrLeaf>> count_;
+};
+
+}  // namespace details
+
+template <std::integral Token, std::integral CountTp>
+[[nodiscard]] constexpr HuffmanTable<Token> MakeHuffmanTable(
+    const Map<Token, CountTp>& count) {}
 
 }  // namespace koda
 
--- include/koda/coders/huffman/huffman_table.tpp
+++ include/koda/coders/huffman/huffman_table.tpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <variant>
+
 namespace koda {
 
 template <std::integral Token>
