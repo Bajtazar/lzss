3ea74b8b8b9df0d4cdfaa267adb9408fac270182
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-11 17:07:45 +0200

    Finished frirst scenario test

--- tests/koda/coders/huffman/huffman_table.test.cpp
+++ tests/koda/coders/huffman/huffman_table.test.cpp
@@ -1,13 +1,16 @@
 #include <koda/coders/huffman/huffman_table.hpp>
 #include <koda/tests/tests.hpp>
 
-
-BeginConstexprTest(HuffmanTable, A) {
+BeginConstexprTest(HuffmanTable, FirstScenario) {
     using HuffmanEntry = koda::HuffmanTable<uint32_t>::entry_type;
 
-    koda::HuffmanTable<uint32_t> kExpected = {
-        HuffmanEntry{0, std::vector<bool>{0}}
-    };
+    const koda::HuffmanTable<uint32_t> kExpected = {
+        HuffmanEntry{0, std::vector<bool>{0}},
+        HuffmanEntry{1, std::vector<bool>{1, 0, 1, 0}},
+        HuffmanEntry{5, std::vector<bool>{1, 1, 1}},
+        HuffmanEntry{16, std::vector<bool>{1, 1, 0}},
+        HuffmanEntry{32, std::vector<bool>{1, 0, 0}},
+        HuffmanEntry{43, std::vector<bool>{1, 0, 1, 1}}};
 
     koda::Map<uint32_t, size_t> res;
     res.Emplace(5, 32);
@@ -17,6 +20,8 @@ BeginConstexprTest(HuffmanTable, A) {
     res.Emplace(43, 16);
     res.Emplace(16, 22);
 
-    auto ves = koda::MakeHuffmanTable(res);
+    auto table = koda::MakeHuffmanTable(res);
+
+    ConstexprAssertEqual(table, kExpected);
 }
 EndConstexprTest;
