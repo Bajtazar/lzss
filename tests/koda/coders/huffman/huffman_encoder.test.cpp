58a601c6148860d4489f2819f514678562fcf85a
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-12 15:20:51 +0200

    Added additional check

--- tests/koda/coders/huffman/huffman_encoder.test.cpp
+++ tests/koda/coders/huffman/huffman_encoder.test.cpp
@@ -4,12 +4,13 @@
 #include <koda/utils/bit_iterator.hpp>
 
 #include <cassert>
+#include <cmath>
 
 static_assert(koda::Encoder<koda::HuffmanEncoder<uint8_t>, uint8_t>);
 
 template <typename Token>
 static constexpr std::vector<bool> ConcatenateSymbols(
-    const koda::HuffmanTable<Token>& table, const std::vector<Token>& tokens) {
+    const koda::HuffmanTable<Token>& table, const auto& tokens) {
     std::vector<bool> result;
     for (const auto& token : tokens) {
         auto iter = table.Find(token);
@@ -42,5 +43,35 @@ BeginConstexprTest(HuffmanEncoderTest, EncodeFirstScenario) {
                         koda::views::LittleEndianOutput);
 
     ConstexprAssertEqual(stream | koda::views::LittleEndianInput, kExpected);
+}
+EndConstexprTest;
+
+BeginConstexprTest(HuffmanEncoderTest, EncodeSecondScenario) {
+    using HuffmanEntry = koda::HuffmanTable<uint32_t>::entry_type;
+
+    const koda::HuffmanTable<char> kTable = {
+        HuffmanEntry{'t', std::vector<bool>{1}},
+        HuffmanEntry{'r', std::vector<bool>{0, 1}},
+        HuffmanEntry{'x', std::vector<bool>{0, 0, 1}},
+        HuffmanEntry{'o', std::vector<bool>{0, 0, 0, 1}},
+        HuffmanEntry{'e', std::vector<bool>{0, 0, 0, 0, 1}},
+        HuffmanEntry{'a', std::vector<bool>{0, 0, 0, 0, 0}}};
+
+    std::string tokens = "trxxaxetrorx";
+    const std::vector<bool> kExpected = ConcatenateSymbols(kTable, tokens);
+
+    koda::HuffmanEncoder encoder{kTable};
+
+    std::vector<char> stream;
+
+    encoder(tokens, stream | koda::views::InsertFromBack |
+                        koda::views::LittleEndianOutput);
+
+    // call operator automatically flushes iter so check whether the flushed
+    // size is valid
+    ConstexprAssertEqual(stream.size(), std::ceil(kExpected.size() / 8.f));
+    ConstexprAssertEqual(stream | koda::views::LittleEndianInput |
+                             koda::views::Take(kExpected.size()),
+                         kExpected);
 }
 EndConstexprTest;
