12ee2768f6fc6e8108348b447706fe3c87c1226c
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-14 21:17:38 +0200

    Added BuildNumberOfBitsMap

--- include/koda/coders/tans/tans_encoder.hpp
+++ include/koda/coders/tans/tans_encoder.hpp
@@ -5,6 +5,7 @@
 #include <koda/collections/map.hpp>
 #include <koda/utils/utils.hpp>
 
+#include <cassert>
 #include <cmath>
 
 namespace koda {
@@ -25,6 +26,23 @@ class TansEncoder : public EncoderInterface<Token, TansEncoder<Token, Count>> {
         }
 
         return saturation_map;
+    }
+
+    static constexpr Map<Count, uint8_t> BuildNumberOfBitsMap(
+        const TansInitTable<Token, Count>& init_table) {
+        auto saturation_map = BuildSaturationMap(init_table);
+
+        return {BuildSaturationMap(init_table) |
+                std::views::transform([&](const auto& saturation_tuple) {
+                    const auto& [token, saturation] = saturation_tuple;
+
+                    auto count_iter = init_table.counts().Find(token);
+                    assert(count_iter != init_table.end());
+
+                    return {token,
+                            2 * init_table.state_sentinel() * saturation -
+                                count_iter->second * (1uz << saturation)};
+                })};
     }
 };
 
