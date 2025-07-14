946e629bb01c256aac5597a4837bca09eda91911
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-14 22:51:55 +0200

    Added UniformDistributionStrided test

--- tests/koda/coders/tans/tans_table.test.cpp
+++ tests/koda/coders/tans/tans_table.test.cpp
@@ -66,4 +66,27 @@ BeginConstexprTest(TansTableTest, GeometricDistributionShifted) {
 }
 EndConstexprTest;
 
+BeginConstexprTest(TansTableTest, UniformDistributionStrided) {
+    koda::Map<char, size_t> counter = {{{'a', 2},
+                                        {'b', 2},
+                                        {'c', 2},
+                                        {'d', 2},
+                                        {'e', 2},
+                                        {'f', 2},
+                                        {'g', 2},
+                                        {'h', 2}}};
+
+    const std::vector<char> kExpectedStateTable = {'b', 'e', 'a', 'd', 'h', 'c', 'g', 'b', 'f', 'a', 'e', 'h', 'd', 'g', 'c', 'f'};
+
+    koda::Map<char, size_t> kExpectedStatesPerToken = counter;
+    const size_t kNumberOfStates = 16;
+
+    koda::TansInitTable table{counter, 2, 7};
+
+    ConstexprAssertEqual(table.state_table(), kExpectedStateTable);
+    ConstexprAssertEqual(table.states_per_token(), kExpectedStatesPerToken);
+    ConstexprAssertEqual(table.number_of_states(), kNumberOfStates);
+}
+EndConstexprTest;
+
 }  // namespace koda
