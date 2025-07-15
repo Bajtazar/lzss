261e64a315ff21865f6d6ffa810701425a8a5f63
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-16 00:33:41 +0200

    Moved logic to tpp file

--- include/koda/coders/tans/tans_encoder.hpp
+++ include/koda/coders/tans/tans_encoder.hpp
@@ -14,54 +14,14 @@ template <typename Token, typename Count>
 class TansEncoder : public EncoderInterface<Token, TansEncoder<Token, Count>> {
    public:
     constexpr explicit TansEncoder(
-        const TansInitTable<Token, Count>& init_table)
-        : saturation_map_{BuildSaturationMap(init_table)},
-          offset_map_{BuildStartOffsetMap(init_table)},
-          renorm_map_{
-              BuildRenormalizationOffsetMap(init_table, saturation_map_)},
-          encoding_table_{BuildEncodingTable(init_table, offset_map_)},
-          state_{init_table.number_of_states()} {}
+        const TansInitTable<Token, Count>& init_table);
 
-    constexpr float TokenBitSize(Token token) const {
-        return saturation_map_.At(token);
-    }
+    constexpr float TokenBitSize(Token token) const;
 
     constexpr auto Encode(InputRange<Token> auto&& input,
-                          BitOutputRange auto&& output) {
-        auto sentinel = std::ranges::end(output);
-        auto iter = FlushEmitter(std::ranges::begin(output), sentinel);
+                          BitOutputRange auto&& output);
 
-        if (iter == sentinel || encoding_table_.empty()) {
-            return CoderResult{std::forward<decltype(input)>(input),
-                               std::move(iter), std::move(sentinel)};
-        }
-
-        auto input_iter = std::ranges::begin(input);
-        auto input_sent = std::ranges::end(input);
-
-        for (; (iter != sentinel) && (input_iter != input_sent); ++input_iter) {
-            EncodeToken(*input_iter);
-            iter = FlushEmitter(iter, sentinel);
-        }
-
-        return CoderResult{std::move(input_iter), std::move(input_sent),
-                           std::move(iter), std::move(sentinel)};
-    }
-
-    constexpr auto Flush(BitOutputRange auto&& output) {
-        auto sentinel = std::ranges::end(output);
-        auto iter = FlushEmitter(std::ranges::begin(output), sentinel);
-
-        if (iter == sentinel || encoding_table_.empty()) {
-            return std::ranges::subrange{std::move(iter), std::move(sentinel)};
-        }
-
-        // Encode final state with uniform distrib to simplify process
-        SetEmitter(IntFloorLog2(encoding_table_.size()));
-
-        return std::ranges::subrange{FlushEmitter(std::move(iter), sentinel),
-                                     sentinel};
-    }
+    constexpr auto Flush(BitOutputRange auto&& output);
 
    private:
     using BitIter = LittleEndianInputBitIter<const Count*>;
@@ -76,88 +36,30 @@ class TansEncoder : public EncoderInterface<Token, TansEncoder<Token, Count>> {
     Count state_;
     Count emitted_bits_[1];
 
-    constexpr void EncodeToken(const auto& token) {
-        auto bit_count =
-            (state_ + renorm_map_.At(token)) / (2 * encoding_table_.size());
-        assert(bit_count <= CHAR_BIT * sizeof(Count));
-        SetEmitter(bit_count);
-        state_ = encoding_table_[offset_map_.At(token) + (state_ >> bit_count)];
-    }
+    constexpr auto EncodeTokens(InputRange<Token> auto&& input, auto iter,
+                                auto sentinel);
 
-    constexpr void SetEmitter(Count bit_count) {
-        emitted_bits_[0] = state_;
-        BitIter start_iter{std::ranges::begin(emitted_bits_)};
-        emitter_ = std::pair{start_iter, std::next(start_iter, bit_count)};
-    }
+    constexpr void EncodeToken(const auto& token);
 
-    constexpr auto FlushEmitter(auto output_iter, const auto& output_sent) {
-        auto& bit_iter = emitter_.first;
-        const auto& bit_sent = emitter_.second;
+    constexpr void SetEmitter(Count bit_count);
 
-        for (; (bit_iter != bit_sent) && (output_iter != output_sent);
-             ++output_iter, ++bit_iter) {
-            *output_iter = *bit_iter;
-        }
-        return output_iter;
-    }
+    constexpr auto FlushEmitter(auto output_iter, const auto& output_sent);
 
     static constexpr Map<Token, Count> BuildSaturationMap(
-        const TansInitTable<Token, Count>& init_table) {
-        Map<Token, Count> saturation_map;
-
-        for (const auto& [token, count] : init_table.states_per_token()) {
-            saturation_map.Emplace(
-                token, static_cast<Count>(std::ceil(std::log2(
-                           static_cast<double>(init_table.number_of_states()) /
-                           count))));
-        }
-
-        return saturation_map;
-    }
+        const TansInitTable<Token, Count>& init_table);
 
     static constexpr Map<Token, uint8_t> BuildRenormalizationOffsetMap(
         const TansInitTable<Token, Count>& init_table,
-        const Map<Token, Count>& saturation_map) {
-        return Map<Token, uint8_t>{
-            BuildSaturationMap(init_table) |
-            std::views::transform([&](const auto& saturation_tuple) {
-                const auto& [token, saturation] = saturation_tuple;
-                auto max = 2 * init_table.number_of_states() * saturation;
-                auto min = init_table.states_per_token().At(token)
-                           << saturation;
-                return std::pair{token, max > min ? (max - min) : 0};
-            })};
-    }
+        const Map<Token, Count>& saturation_map);
 
     static constexpr Map<Token, SCount> BuildStartOffsetMap(
-        const TansInitTable<Token, Count>& init_table) {
-        Map<Token, SCount> offset_map;
-
-        Count accumulator = 0;
-        for (const auto& [token, count] : init_table.states_per_token()) {
-            offset_map.Emplace(token, static_cast<SCount>(accumulator) - count);
-            accumulator += count;
-        }
-
-        return offset_map;
-    }
+        const TansInitTable<Token, Count>& init_table);
 
     static constexpr std::vector<Count> BuildEncodingTable(
         const TansInitTable<Token, Count>& init_table,
-        const Map<Token, SCount>& start_offset_map) {
-        const auto number_of_states = init_table.number_of_states();
-        std::vector<Count> encoding_table(number_of_states);
-        Map<Token, Count> next = init_table.states_per_token();
-
-        for (Count i = 0; i < number_of_states; ++i) {
-            const auto& token = init_table.state_table()[i];
-
-            encoding_table[next.At(token)++ + start_offset_map.At(token)] =
-                i + number_of_states;
-        }
-
-        return encoding_table;
-    }
+        const Map<Token, SCount>& start_offset_map);
 };
 
 }  // namespace koda
+
+#include <koda/coders/tans/tans_encoder.tpp>
