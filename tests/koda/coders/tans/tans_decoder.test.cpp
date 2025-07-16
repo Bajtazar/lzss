937055c8d9f5e6bed96a74374b8efae72ae03c00
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-16 22:01:53 +0200

    Added UniformDistributionStridedAndScaled test

--- tests/koda/coders/tans/tans_decoder.test.cpp
+++ tests/koda/coders/tans/tans_decoder.test.cpp
@@ -84,3 +84,32 @@ BeginConstexprTest(TansDecoderTest, UniformDistributionStrided) {
     ConstexprAssertEqual(result | std::views::reverse, kExpected);
 }
 EndConstexprTest;
+
+BeginConstexprTest(TansDecoderTest, UniformDistributionStridedAndScaled) {
+    const koda::Map<char, size_t> kCounter = {{{'a', 2},
+                                               {'b', 2},
+                                               {'c', 2},
+                                               {'d', 2},
+                                               {'e', 2},
+                                               {'f', 2},
+                                               {'g', 2},
+                                               {'h', 2}}};
+
+    koda::TansInitTable table{kCounter, 2, 7, 13};
+    koda::TansDecoder decoder{table};
+
+    const std::vector<bool> kStream = {1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1,
+                                       1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1,
+                                       1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+                                       0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0};
+
+    const std::string kExpected = "abacdaeffagggaah";
+
+    std::string result;
+
+    decoder(kExpected.size(), kStream | std::views::reverse,
+            result | koda::views::InsertFromBack);
+
+    ConstexprAssertEqual(result | std::views::reverse, kExpected);
+}
+EndConstexprTest;
