bc81800776f137a5636d2faf909f33e52d2fa64b
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-16 21:10:26 +0200

    Added first tans decoder test

--- tests/koda/coders/tans/tans_decoder.test.cpp
+++ tests/koda/coders/tans/tans_decoder.test.cpp
@@ -7,3 +7,29 @@
 #include <cinttypes>
 
 static_assert(koda::Decoder<koda::TansDecoder<uint8_t, size_t>, uint8_t>);
+
+BeginConstexprTest(TansDecoderTest, UniformDistribution) {
+    const koda::Map<char, size_t> kCounter = {{{'a', 2},
+                                               {'b', 2},
+                                               {'c', 2},
+                                               {'d', 2},
+                                               {'e', 2},
+                                               {'f', 2},
+                                               {'g', 2},
+                                               {'h', 2}}};
+    koda::TansInitTable table{kCounter};
+    koda::TansDecoder decoder{table};
+
+    const std::vector<bool> kStream = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                                       0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,
+                                       0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1,
+                                       1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1};
+    const std::string kExpected = "abacdaeffagggaah";
+
+    std::string result;
+
+    decoder(kStream, result | koda::views::InsertFromBack);
+
+    ConstexprAssertEqual(result, kExpected);
+}
+EndConstexprTest;
