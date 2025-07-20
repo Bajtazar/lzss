85729e8e24e416cf6aaa57322afda69fdf4a3b4e
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-20 23:33:26 +0200

    Added first serious asymetrical test

--- tests/koda/coders/lz77/lz77_encoder.asymetric.test.cpp
+++ tests/koda/coders/lz77/lz77_encoder.asymetric.test.cpp
@@ -3,63 +3,37 @@
 #include <koda/ranges/bit_iterator.hpp>
 #include <koda/tests/tests.hpp>
 
+#include <gtest/gtest.h>
+
+#include <print>
+
+#include "common.hpp"
+
 static_assert(koda::Encoder<koda::Lz77Encoder<uint8_t>, uint8_t>);
 
-namespace {
-
-template <typename Tp>
-struct Lz77DummyAuxEncoder
-    : public koda::EncoderInterface<Tp, Lz77DummyAuxEncoder<Tp>> {
-    using token_type = Tp;
-    using asymetric = void;
-
-    constexpr explicit Lz77DummyAuxEncoder() = default;
-
-    std::vector<Tp> tokens = {};
-
-    [[noreturn]] constexpr float TokenBitSize([[maybe_unused]] Tp token) const {
-        throw std::runtime_error{"Unused expression!"};
-    }
-
-    constexpr auto Encode(koda::InputRange<Tp> auto&& input,
-                          koda::BitOutputRange auto&& output) {
-        tokens.insert_range(tokens.end(), std::forward<decltype(input)>(input));
-        // Circumvent static analysis
-        auto iter = std::ranges::begin(input);
-        const auto sent = std::ranges::end(input);
-        for (; iter != sent; ++iter);
-        return koda::CoderResult{iter, sent,
-                                 std::forward<decltype(output)>(output)};
-    }
-
-    constexpr auto Flush(koda::BitOutputRange auto&& output) {
-        return std::forward<decltype(output)>(output);
-    }
-};
-
-}
+using DummyEncoder =
+    Lz77DummyAuxEncoder<koda::Lz77IntermediateToken<char>, true>;
 
 BeginConstexprTest(Lz77EncoderAsymetricTest, EncodeTokens) {
     std::string input_sequence = "ala ma kota a kot ma ale";
     std::vector expected_result = {
-        koda::Lz77IntermediateToken<char>{'a', 0, 0},  // 'a'
-        koda::Lz77IntermediateToken<char>{'l', 0, 0},  // 'l'
-        koda::Lz77IntermediateToken<char>{' ', 0, 1},  // 'a '
-        koda::Lz77IntermediateToken<char>{'m', 0, 0},  // 'm'
-        koda::Lz77IntermediateToken<char>{'k', 2, 2},  // 'a k'
-        koda::Lz77IntermediateToken<char>{'o', 0, 0},  // 'o'
-        koda::Lz77IntermediateToken<char>{'t', 0, 0},  // 't'
-        koda::Lz77IntermediateToken<char>{'a', 5, 2},  // 'a a'
-        koda::Lz77IntermediateToken<char>{' ', 6, 4},  // ' kot '
-        koda::Lz77IntermediateToken<char>{'a', 4, 3},  // 'ma a'
-        koda::Lz77IntermediateToken<char>{'e', 1, 1},  // 'le'
+        koda::Lz77IntermediateToken<char>{'a', 1, 2},  // 'a'
+        koda::Lz77IntermediateToken<char>{'k', 3, 4},  // 'l'
+        koda::Lz77IntermediateToken<char>{'a', 7, 2},  // 'a '
+        koda::Lz77IntermediateToken<char>{' ', 2, 2},  // 'm'
+        koda::Lz77IntermediateToken<char>{'k', 9, 0},  // 'a k'
+        koda::Lz77IntermediateToken<char>{'o', 8, 0},  // 'o'
+        koda::Lz77IntermediateToken<char>{'t', 7, 0},  // 't'
+        koda::Lz77IntermediateToken<char>{'m', 3, 1},  // 'a a'
+        koda::Lz77IntermediateToken<char>{' ', 2, 1},  // ' kot '
+        koda::Lz77IntermediateToken<char>{'a', 2, 0},  // 'ma a'
+        koda::Lz77IntermediateToken<char>{'l', 1, 0},  // 'le'
+        koda::Lz77IntermediateToken<char>{'e', 0, 0},  // 'le'
     };
 
     std::vector<uint8_t> target;
 
-    koda::Lz77Encoder<char,
-                      Lz77DummyAuxEncoder<koda::Lz77IntermediateToken<char>>>
-        encoder{1024, 4};
+    koda::Lz77Encoder<char, DummyEncoder> encoder{1024, 4};
     encoder(input_sequence, target | koda::views::InsertFromBack |
                                 koda::views::LittleEndianOutput);
 
@@ -96,8 +70,8 @@ EndConstexprTest;
 //     encoder(input_sequence, target | koda::views::InsertFromBack |
 //                                 koda::views::LittleEndianOutput);
 
-//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens, expected_result);
-//     ConstexprAssertTrue(target.empty());
+//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens,
+//     expected_result); ConstexprAssertTrue(target.empty());
 // }
 // EndConstexprTest;
 
@@ -124,8 +98,8 @@ EndConstexprTest;
 //     encoder(input_sequence, target | koda::views::InsertFromBack |
 //                                 koda::views::LittleEndianOutput);
 
-//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens, expected_result);
-//     ConstexprAssertTrue(target.empty());
+//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens,
+//     expected_result); ConstexprAssertTrue(target.empty());
 // }
 // EndConstexprTest;
 
@@ -147,8 +121,8 @@ EndConstexprTest;
 //         koda::Lz77IntermediateToken<char>{'i', 0, 0},  // [abcdefgh|ijk]
 //         koda::Lz77IntermediateToken<char>{'j', 0, 0},  // [bcdefghi|jke]
 //         koda::Lz77IntermediateToken<char>{'k', 0, 0},  // [cdefghij|kel]
-//         koda::Lz77IntermediateToken<char>{'l', 1, 1},  // [defghijk|elm] e = 1,1
-//         koda::Lz77IntermediateToken<char>{'m', 0, 0},  // [fghijkel|mou]
+//         koda::Lz77IntermediateToken<char>{'l', 1, 1},  // [defghijk|elm] e =
+//         1,1 koda::Lz77IntermediateToken<char>{'m', 0, 0},  // [fghijkel|mou]
 //         koda::Lz77IntermediateToken<char>{'o', 0, 0},  // [ghijkelm|oup]
 //         koda::Lz77IntermediateToken<char>{'u', 0, 0},  // [hijkelmo|upr]
 //         koda::Lz77IntermediateToken<char>{'p', 0, 0},  // [ijkelmou|prs]
@@ -169,8 +143,8 @@ EndConstexprTest;
 //     encoder(input_sequence, target | koda::views::InsertFromBack |
 //                                 koda::views::LittleEndianOutput);
 
-//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens, expected_result);
-//     ConstexprAssertTrue(target.empty());
+//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens,
+//     expected_result); ConstexprAssertTrue(target.empty());
 // }
 // EndConstexprTest;
 
@@ -188,7 +162,7 @@ EndConstexprTest;
 //     encoder(input_sequence, target | koda::views::InsertFromBack |
 //                                 koda::views::LittleEndianOutput);
 
-//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens, expected_result);
-//     ConstexprAssertTrue(target.empty());
+//     ConstexprAssertEqual(encoder.auxiliary_encoder().tokens,
+//     expected_result); ConstexprAssertTrue(target.empty());
 // }
 // EndConstexprTest;
