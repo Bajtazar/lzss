0276e3dcbefe1cdada678e7223642f72f73b84f6
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-07-21 22:35:55 +0200

    Started working on lzss intermediate token decoder

---
+++ include/koda/coders/lzss/lzss_intermediate_token_decoder.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <koda/coders/coder.hpp>
+#include <koda/coders/coder_traits.hpp>
+#include <koda/coders/lzss/lzss_intermediate_token.hpp>
+
+namespace koda {
+
+template <std::integral InputToken, UnsignedIntegral PositionTp,
+          UnsignedIntegral LengthTp, Decoder<InputToken> TokenDecoder,
+          Decoder<PositionTp> PositionDecoder, Decoder<LengthTp> LengthDecoder>
+class LzssIntermediateTokenDecoder
+    : public DecoderInterface<
+          LzssIntermediateToken<InputToken, PositionTp, LengthTp>,
+          DecoderInterface<LzssIntermediateTokenDecoder<
+              InputToken, PositionTp, LengthTp, TokenDecoder, PositionDecoder,
+              LengthDecoder>>> {
+    using TokenTraits = CoderTraits<TokenDecoder>;
+    using PositionTraits = CoderTraits<PositionDecoder>;
+    using LengthTraits = CoderTraits<LengthDecoder>;
+
+   public:
+    using token_type = LzssIntermediateToken<InputToken, PositionTp, LengthTp>;
+
+    static constexpr bool IsAsymetric = TokenTraits::IsAsymetric &&
+                                        PositionTraits::IsAsymetric &&
+                                        LengthTraits::IsAsymetric;
+
+    constexpr explicit LzssIntermediateTokenDeoder(
+        TokenDecoder token_decoder, PositionDecoder position_decoder,
+        LengthDecoder length_decoder);
+    constexpr auto Initialize(BitInputRange auto&& input);
+
+    constexpr auto Decode(BitInputRange auto&& input,
+                          std::ranges::output_range<Token> auto&& output);
+
+   private:
+    enum class State : uint8_t {
+        kBit = 0,
+        kToken = 1,
+        kPosition = 2,
+        kLength = 3
+    };
+
+    TokenDecoder token_decoder_;
+    PositionDecoder position_decoder_;
+    LengthDecoder length_decoder_;
+    State state_ = State::kBit;
+
+    static constexpr bool IsSymetric = TokenTraits::IsSymetric &&
+                                       PositionTraits::IsSymetric &&
+                                       LengthTraits::IsSymetric;
+
+    static_assert(IsSymetric || IsAsymetric,
+                  "All of the decoders has to be either symetric or asymetric");
+};
+
+}  // namespace koda
